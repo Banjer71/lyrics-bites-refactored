d78f063e98801a1cb3f5766ea8ce8f4f8cd30128
Banjer71 <davgio7@gmail.com>
2020-02-08 22:16:44 +0000

    upload the basic structure with no css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SearchBar from './SearchBar';
+import SongPage from './SongPage';
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Lyrics Bites</h1>
+      <h2>Learn your favourite song one bite at a time</h2>
+			<Router>
+				<Route path="/" exact component={SearchBar} />
+				<Route path="/SongPage" exact component={SongPage} />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
